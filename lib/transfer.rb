f9f2ebeba269aed774aef9699483137be6813f18
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-19 14:31:13 -0800

    partial complete for Charlie

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,18 @@
+#require_relative '../lib/bank_account'
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  attr_accessor :status, :amount 
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver 
+    @status = 'pending'
+    @amount = 50 
+  end 
+
+  def valid?
+    self.sender.valid? && self.receiver.valid? 
+  end 
 end
